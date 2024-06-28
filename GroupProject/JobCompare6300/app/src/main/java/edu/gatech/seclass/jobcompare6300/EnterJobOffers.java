7f5adee196d331ec686545a393a4b937eac52e4a
Thu Nguyen <thuminhnguyen@berkeley.edu>
2024-06-28 16:55:48 -0700

    Final cleaned up for Enter/Edit Current Job

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterJobOffers.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterJobOffers.java
@@ -1,26 +1,24 @@
 package edu.gatech.seclass.jobcompare6300;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class EnterJobOffers extends AppCompatActivity {
-    private EditText inputTitle, inputCompany, inputState, inputCity,inputLivingCost,
-            inputYearlySalary,inputYearlyBonus,inputTrainingDevelopment,inputLeaveTime, inputTelework;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private String title, company, locationState, locationCity, costOfLiving, yearlySalary, yearlyBonus, trainDevFund, leaveTime, teleworkDay;
-    private Integer costOfLivingInt, yearlySalaryInt, yearlyBonusInt, trainDevFundInt, leaveTimeInt, teleworkDayInt;
+public class EnterJobOffers extends AppCompatActivity {
+    private EditText inputTitle, inputCompany, inputState, inputCity, inputLivingCost,
+            inputYearlySalary, inputYearlyBonus, inputTrainingDevelopment, inputLeaveTime, inputTelework;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_job_offers);
+
         inputTitle = findViewById(R.id.inputTitleID);
         inputCompany = findViewById(R.id.inputCompanyID);
         inputState = findViewById(R.id.inputStateID);
@@ -31,94 +29,60 @@ public class EnterJobOffers extends AppCompatActivity {
         inputTrainingDevelopment = findViewById(R.id.inputTrainingDevelopmentID);
         inputLeaveTime = findViewById(R.id.inputLeaveTimeID);
         inputTelework = findViewById(R.id.inputTeleworkID);
-        Button buttonSaveJob = findViewById(R.id.saveJobButtonID);
-//        Button backToMainMenuButtonID = findViewById(R.id.backToMainMenuButtonID);
-
-        buttonSaveJob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                title = inputTitle.getText().toString().trim();
-                company = inputCompany.getText().toString().trim();
-                locationState = inputState.getText().toString().trim();
-                locationCity = inputCity.getText().toString().trim();
-                costOfLiving = inputLivingCost.getText().toString().trim();
-                yearlySalary = inputYearlySalary.getText().toString().trim();
-                yearlyBonus = inputYearlyBonus.getText().toString().trim();
-                trainDevFund = inputTrainingDevelopment.getText().toString().trim();
-                leaveTime = inputLeaveTime.getText().toString().trim();
-                teleworkDay = inputTelework.getText().toString().trim();
-
-                boolean isValid = checkIfValid(title, company, locationState, locationCity,
-                        costOfLiving, yearlySalary, yearlyBonus, trainDevFund,leaveTime, teleworkDay);
-                if(!isValid)
-                {
-                    return;
-                }
-
-                costOfLivingInt = Integer.parseInt(costOfLiving);
-                yearlySalaryInt = Integer.parseInt(yearlySalary);
-                yearlyBonusInt = Integer.parseInt(yearlyBonus);
-                trainDevFundInt = Integer.parseInt(trainDevFund);
-                leaveTimeInt = Integer.parseInt(leaveTime);
-                teleworkDayInt = Integer.parseInt(teleworkDay);
-
-                handleClickSaveJob(v);
-            }
-        });
     }
 
     private boolean checkIfValid(String title, String company, String locationState, String locationCity, String livingCost,
                                  String yearlySalary, String yearlyBonus, String trainingFund, String leaveTime, String teleworkDays) {
         boolean isValid = true;
-        if(title.isEmpty()){
+        if (title.isEmpty()) {
             inputTitle.setError("Title is empty");
             isValid = false;
         }
-        if(company.isEmpty()){
+        if (company.isEmpty()) {
             inputCompany.setError("Company is empty");
             isValid = false;
         }
-        if(locationState.isEmpty()){
+        if (locationState.isEmpty()) {
             inputState.setError("Location - State is empty");
             isValid = false;
         }
-        if(locationCity.isEmpty()){
+        if (locationCity.isEmpty()) {
             inputCity.setError("Location - City is empty");
             isValid = false;
         }
-        if(livingCost.isEmpty()){
+        if (livingCost.isEmpty()) {
             inputLivingCost.setError("Cost of Living Index is empty");
             isValid = false;
         }
-        if(yearlySalary.isEmpty()){
+        if (yearlySalary.isEmpty()) {
             inputYearlySalary.setError("Yearly Salary is empty");
             isValid = false;
         }
-        if(yearlyBonus.isEmpty()){
+        if (yearlyBonus.isEmpty()) {
             inputYearlyBonus.setError("Yearly Bonus is empty");
             isValid = false;
         }
-        if(trainingFund.isEmpty()){
+        if (trainingFund.isEmpty()) {
             inputTrainingDevelopment.setError("Training Fund is empty");
             isValid = false;
         }
-        if(Integer.parseInt(trainingFund) < 0 || Integer.parseInt(trainingFund) > 18000){
+        if (Integer.parseInt(trainingFund) < 0 || Integer.parseInt(trainingFund) > 18000) {
             inputTrainingDevelopment.setError("$0 to $18000 inclusive annually");
             isValid = false;
         }
-        if(leaveTime.isEmpty()){
+        if (leaveTime.isEmpty()) {
             inputLeaveTime.setError("Leave Time is empty");
             isValid = false;
         }
-        if(Integer.parseInt(leaveTime) < 0 || Integer.parseInt(leaveTime) > 100){
+        if (Integer.parseInt(leaveTime) < 0 || Integer.parseInt(leaveTime) > 100) {
             inputTrainingDevelopment.setError("0 - 100 inclusive annually");
             isValid = false;
         }
-        if(teleworkDays.isEmpty()){
+        if (teleworkDays.isEmpty()) {
             inputTelework.setError("Telework Days is empty");
             isValid = false;
         }
-        if(Integer.parseInt(teleworkDays) < 0 || Integer.parseInt(teleworkDays) > 5){
+        if (Integer.parseInt(teleworkDays) < 0 || Integer.parseInt(teleworkDays) > 5) {
             inputTrainingDevelopment.setError("0 - 5 inclusive weekly");
             isValid = false;
         }
@@ -130,6 +94,31 @@ public class EnterJobOffers extends AppCompatActivity {
     }
 
     public void handleClickSaveJob(View view) {
+
+        String title = inputTitle.getText().toString().trim();
+        String company = inputCompany.getText().toString().trim();
+        String locationState = inputState.getText().toString().trim();
+        String locationCity = inputCity.getText().toString().trim();
+        String costOfLiving = inputLivingCost.getText().toString().trim();
+        String yearlySalary = inputYearlySalary.getText().toString().trim();
+        String yearlyBonus = inputYearlyBonus.getText().toString().trim();
+        String trainDevFund = inputTrainingDevelopment.getText().toString().trim();
+        String leaveTime = inputLeaveTime.getText().toString().trim();
+        String teleworkDay = inputTelework.getText().toString().trim();
+
+        boolean isValid = checkIfValid(title, company, locationState, locationCity,
+                costOfLiving, yearlySalary, yearlyBonus, trainDevFund, leaveTime, teleworkDay);
+        if (!isValid) {
+            return;
+        }
+
+        Integer costOfLivingInt = Integer.parseInt(costOfLiving);
+        Integer yearlySalaryInt = Integer.parseInt(yearlySalary);
+        Integer yearlyBonusInt = Integer.parseInt(yearlyBonus);
+        Integer trainDevFundInt = Integer.parseInt(trainDevFund);
+        Integer leaveTimeInt = Integer.parseInt(leaveTime);
+        Integer teleworkDayInt = Integer.parseInt(teleworkDay);
+
         // Instantiate our subclass of SQLiteOpenHelper
         JobDbHelper dbHelper = new JobDbHelper(this);
 
